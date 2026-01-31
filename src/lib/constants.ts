99a153edcad0fe72153bfb8abac30b6fbebc84f0
ClintonEmok <clintonneemok11@gmail.com>
2026-01-31 01:41:44 +0100

    feat(02-01): define time control constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const TIME_MIN = 0;
+export const TIME_MAX = 100;
+export const TIME_STEP_DEFAULT = 1;
+export const PLAYBACK_SPEED_DEFAULT = 10; // units per second
+export const TIME_WINDOW_DEFAULT = 5;
